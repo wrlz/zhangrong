ab53847a78516a2c56cd5501edd77369a7c96b41
wrlz <32238011+wrlz@users.noreply.github.com>
2017-09-24 21:03:31 +0800

    Create demo21.java

---
+++ demo21.java
@@ -0,0 +1,12 @@
+public class demo21 {
+public static void main(String []args){
+	String stuName = "罗樟";
+    char stuSex = '男';
+    int stuAge = 19;
+    boolean isHunan = true;
+    System.out.println("姓名"+ stuName);
+    System.out.println("性别"+ stuSex);
+    System.out.println("年龄"+ stuAge); 
+    System.out.println("是否为湖南籍："+ isHunan);
+}
+}
